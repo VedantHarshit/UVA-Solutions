2b6bf3a80f0a6be98de6f579a7b50f00a0651abf
Vedant Harshit <53620816+VedantHarshit@users.noreply.github.com>
2020-12-24 21:11:21 +0530

    Add files via upload

---
+++ HappyBirthdaySong.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+
+public class Main
+{
+	public static void main(String[] args)throws IOException
+	{ 
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(input.readLine());
+
+		String[] names = new String[n];
+
+		for(int i = 0 ; i < n ; i++)
+			names[i] = input.readLine();
+
+		int sp = 0;
+
+		String[] song = {"Happy", "birthday", "to", "you", "Happy", "birthday", "to", "you", "Happy", "birthday", "to", "Rujia", "Happy", "birthday", "to", "you"};
+
+
+		for(int i = 0 ; i < n ; i++)
+		{
+			System.out.println(names[i] + ": " + song[sp]);
+			sp++;
+			if(sp == song.length)
+				sp = 0;
+		}
+
+		if(sp != 0)
+		{
+			int index = 0;
+			while(sp < song.length)
+			{
+				System.out.println(names[index] + ": " + song[sp]);
+				index++;
+				sp++;
+				if(index == n)
+					index = 0;
+			}
+		}
+	} 
+}
